fca47b2c5302e9b10de996b5209d783355d81b72
ewhalgand <halgandewen44@gmail.com>
2025-05-15 03:18:22 +0200

    fix: reset cookie name

--- apps/backend/src/auth/auth.ts
+++ apps/backend/src/auth/auth.ts
@@ -12,9 +12,6 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
-  advanced: {
-    cookiePrefix: "my-app",
-  },
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
